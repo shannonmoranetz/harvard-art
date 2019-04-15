1a72d5e35ec3eca976001c5034b4125d8a97910f
Shannon Moranetz <shannonrmoranetz@gmail.com>
2019-04-15 13:59:04 -0600

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ArtSection />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ArtSection from './components/ArtSection.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    ArtSection
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
 }
 </style>
---
+++ src/components/ArtSection.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="ArtSection">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ArtSection',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
